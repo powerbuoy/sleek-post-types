d91c85f725d6da48b2cfe9c961c24b35b5650d66
AL <powerbuoy@gmail.com>
2019-09-23 12:58:40 +0200

    Moar

--- register.php
+++ register.php
@@ -7,9 +7,21 @@ add_action('init', function () {
 	# Include all PostType classes
 	if (file_exists(get_stylesheet_directory() . '/post-types/')) {
 		foreach (glob(get_stylesheet_directory() . '/post-types/*.php') as $file) {
+			$filename = substr(basename($file), 0, -4);
+			$className = str_replace('-', '', ucwords($filename, '-'));
+			$ptName = str_replace('-', '_', $filename);
+
+			$postTypes[$ptName] = $className;
+
 			require_once $file;
 		}
 	}
 
 	# Go through
+	foreach ($postTypes as $postType => $className) {
+		$fullClassName = "Sleek\PostTypes\\$className";
+		$pt = new $fullClassName(null);
+
+		var_dump($pt->config());
+	}
 });
