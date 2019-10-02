c558c057502622de3d6a008e9e6fed7452309374
AL <powerbuoy@gmail.com>
2019-10-02 08:56:24 +0200

    cleanup

--- sleek-post-type.php
+++ sleek-post-type.php
@@ -8,11 +8,6 @@ abstract class PostType {
 		$this->postId = $postId;
 	}
 
-	# TODO: Use magic getter instead?
-	public function get_field ($name) {
-		return get_field($name, $this->postId);
-	}
-
 	public function created () {
 
 	}
