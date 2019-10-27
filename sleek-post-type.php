ec6f6ce946f4f6b8d8d854073af380db3ade9a2e
AL <powerbuoy@gmail.com>
2019-10-27 14:36:27 +0100

    Docs

--- sleek-post-type.php
+++ sleek-post-type.php
@@ -8,18 +8,22 @@ abstract class PostType {
 		$this->postId = $postId;
 	}
 
+	# Lifecycle hook
 	public function created () {
 
 	}
 
+	# PostType config
 	public function config () {
 		return [];
 	}
 
+	# Returns all fields and potential defaults for this module
 	public function fields () {
 		return [];
 	}
 
+	# Returns all fields before they're sent to ACF
 	public function get_fields ($acfKey = null) {
 		return apply_filters('sleek_post_type_fields', $this->fields());
 	}
